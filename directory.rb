61f7f8f0d4694d99cfb7e5c611347d081e234deb
Slaiman <sulaimanahmad321@gmail.com>
2017-03-29 18:48:32 +0100

    Added everything in exercise 7 + added center method to a few more strings

--- directory.rb
+++ directory.rb
@@ -1,18 +1,35 @@
 # let's put all students inot an array
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students and their cohort on the next line"
+  puts "To finish, type 'quit' and hit enter twice"
   # create an empty array
   students = []
   #get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
+  name = "no name was given" if name.empty?
+
+  cohort = gets.chomp.downcase
+  cohort = "no cohort was given" if cohort.empty?
+  months = %w(january february march april may june july august september october november december )
+  months << "no cohort was given"
+  while !months.include? cohort
+    puts "Please type the correct cohort"
+    cohort = gets.chomp.downcase
+  end
+
+  while name != 'quit' do
     # add the student has to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort.gsub(/\s+/,"_").downcase.to_sym}
     puts "Now we have #{students.count} students"
     # get another name from user
     name = gets.chomp
+    name = "no name was given" if name.empty?
+    cohort = gets.chomp.downcase
+    cohort = "no cohort was given" if cohort.empty?
+    while !months.include? cohort
+      puts "Please type the correct cohort"
+      cohort = gets.chomp.downcase
+    end
   end
   # return the array of students
   students
@@ -20,22 +37,21 @@ end
 
 
 def print_header
-puts "The students of Villians Academy"
-puts "-------------"
+puts "The students of Villians Academy".center(50)
+puts "--------------------------".center(50)
 end
 
 def print(students)
-i = 0
-while i < students.count
-
-  str = "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
-  puts str.center(str.length + 15)
-  i += 1
-end
+  i = 0
+  while i < students.count
+    str = "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts str.center(str.length + 15)
+    i += 1
+  end
 end
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+puts "Overall, we have #{students.count} great students".center(50)
 end
 #nothing happend until we call methods
 students = input_students
